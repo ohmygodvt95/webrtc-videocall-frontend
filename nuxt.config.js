682115ce0c0a9ec40456f046927e2629ff3be6fb
ThienLV <le.vinh.thien@framgia.com>
2020-04-11 15:59:10 +0900

    add bypass https fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { 'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      // { 'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
